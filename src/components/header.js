1f096488745a0d25746ffe784685349ce3a75dcb
Nikita Nazarov <nnazarov55@gmail.com>
2023-04-04 00:23:41 +0300

    committed

---
+++ src/components/header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function header() {
+  return (
+    <header className='header'>
+        <div className='header-content'>
+        <div className='logo'></div>
+          <nav className='nav-list'>
+            <ul>
+              <li className='li-item'>Cart</li>
+              <li className='li-item'>Conatacts</li>
+              <li className='li-item'>Personal account</li>
+            </ul>
+          </nav>
+        </div>
+        <div className='bg-2'></div>
+        <div className='bg'></div>
+        <div className='baner'></div>
+        
+    </header>
+  )
+}
