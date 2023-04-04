06b61872c905c6c46046076e5e99020454c112c8
Nikita Nazarov <nnazarov55@gmail.com>
2023-04-04 23:41:13 +0300

    ss

--- src/components/ShowFullInformation.js
+++ src/components/ShowFullInformation.js
@@ -4,23 +4,24 @@ export class ShowFullInformation extends Component {
   render() {
     return (
       <div className="full-item">
-        <img
-          onClick={() => this.props.fullDiscript(this.props.item)}
-          className="item-img "
-          src={"./img/" + this.props.item.img}
-          alt=""
-        ></img>
-        <h2 className="h2">{this.props.item.title}</h2>
-        <p className="p">{this.props.item.desc}</p>
-        <p className="p dolar">{this.props.item.price}</p>
-        <div
-          className="add-to-cart"
-          onClick={() => this.props.onAdd(this.props.item)}
-        >
-          Add to cart
+        <div className="full-item-wrapper">
+          <img
+            onClick={() => this.props.fullDiscript(this.props.item)}
+            className="item-img "
+            src={"./img/" + this.props.item.img}
+            alt=""
+          ></img>
+          <h2 className="h2">{this.props.item.title}</h2>
+          <p className="p">{this.props.item.desc}</p>
+          <p className="p dolar">{this.props.item.price}</p>
+          <div
+            className="add-to-cart"
+            onClick={() => this.props.onAdd(this.props.item)}
+          >
+            Add to cart
+          </div>
         </div>
       </div>
-      
     );
   }
 }
